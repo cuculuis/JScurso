6dea50461a5f1e3171f6492d38c8390314bf875e
Luis Angel Lopez Solis <luisangel96@gmail.com>
2022-04-12 19:01:15 -0300

    Commit 3: modificación de funciones

--- 2-SIMULADOR-INTERACTIVO/simulador-interactivo.js
+++ 2-SIMULADOR-INTERACTIVO/simulador-interactivo.js
@@ -1,17 +1,48 @@
 //Variables Usuario//
 let nombre = prompt("Ingresa tu nombre");
 let edad = parseInt(prompt("Ingrese su edad"));
-let mdSueldo = prompt("Si su sueldo es mensual precione 'm' si es por hora precione 'h'");
+let sueldoTotaldeHoras = 0;
+let sueldoMensual = 0;
+let sueldoNetoHora = 0;
+let sueldoNetoMes = 0;
+let sueldoxHora = 0;
+let horasTotales = 0;
+
+
 
 //Constantes de impuestos//
 const impuesto = 0.17
 
-//Pregunta si el sueldo es mensual u horario//
-if (mdSueldo.toLowerCase == "m") {
-    let sueldoM = parseInt(prompt("Ingrese su sueldo mensual en bruto"));
-} else if (mdSueldo.toLowerCase == "h") {
-    let sueldoH = parseInt(prompt("Ingrese el valor bruto de la hora de trabajo"));
-    let hTotal = parseInt(prompt("Ingrese las horas totales trabajadas"));
+//FUNCIONES//
+function saludo () {
+    alert ('Hola ' + nombre + ' su edad es ' + edad + ', ahora va a conocer cuánto gana mensualmente.')
 }
 
+saludo();
 
+//PARA SABER SI SU SUELDO ES POR MES O POR HORA//
+let modoSueldo = prompt("Si su sueldo es mensual precione 'm' si es por hora precione 'h'");
+
+function SueldoTotalMensualdeHoras() {
+    let sueldoTotaldeHoras = sueldoxHora * horasTotales
+}
+
+function calSueldoNetoHora () {
+    let sueldoNetoHora = (sueldoxHora * horasTotales) - (impuesto * (sueldoxHora * horasTotales))
+    alert('Su sueldo total es ' + sueldoNetoHora);
+}
+
+function calSueldoNetoMes () {
+    let sueldoNetoMes = sueldoMensual - (impuesto * sueldoMensual)
+    alert('Su sueldo total es ' + sueldoNetoMes);
+}
+
+//Pregunta si el sueldo es mensual u horario//
+if (modoSueldo.toLowerCase() == "m") {
+    let sueldoMensual = parseInt(prompt("Ingrese su sueldo mensual en bruto"));
+    calSueldoNetoMes();
+} else if (modoSueldo.toLowerCase() == "h") {
+    let sueldoxHora = parseInt(prompt("Ingrese el valor bruto de la hora de trabajo"));
+    let horasTotales = parseInt(prompt("Ingrese las horas totales trabajadas"));
+    calSueldoNetoHora();
+}
