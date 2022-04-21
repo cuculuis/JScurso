72cf665b6873b4f5453b76c4f430b602d5b881e4
Luis Angel Lopez Solis Remoto <luisangel96@gmail.com>
2022-04-21 19:43:40 -0300

    Commit 11: Modificación de algunas cosas en las funciones... Spoiler: siguen sin funcionar.. ja..

--- 4-PRIMERAENTREGA/primeraentrega.js
+++ 4-PRIMERAENTREGA/primeraentrega.js
@@ -40,54 +40,46 @@ class Personaje {
     }
 }
 
-///LANZAMIENTO DE DADO////
-function LanzarDado() {
-    let Dado = 1+Math.round(Math.random()*5);
-    return Dado;
-}
+
+
 
 ///VARIABLES INDEPENDIENTES///
 let Recorrido = 0
 let Camino = 30
+let Dado = 1+Math.round(Math.random()*5);
 
-///CAMINAR///
-function Caminar() {
-    let Pasos = 0
-    Pasos = LanzarDado()
-    console.log('Has dado ' + Pasos + ' pasos.');
-    return Pasos;
-}
 
 ///DESPLAZAMIENTO///
 function Desplazamiento() {
-    Recorrido = Camino - Caminar()
+    Recorrido = Camino - Dado
     console.log('Te faltan recorrer ' + Recorrido + ' pasos.');
 } 
 
 ///DATOS DE PERSONAJE///
-let Jugar = prompt('¿Quieres jugar? (Y/N)').toUpperCase()
-let Username = prompt("Introduzca su nombre de usuario").toUpperCase()
+let Jugar = prompt('¿Quieres jugar? Por favor escriba "Y" o "N"').toUpperCase()
 const Estatus = []
-Estatus.push(new Personaje(TipodePersonaje = prompt('Escoja su personaje... Escriba "Verde", "Azul" o "Rojo"').toUpperCase()))
 
 if (Jugar == "Y") {
-    do {
-        LanzarDado()
-
-        switch (LanzarDado()) {
+    let Username = prompt("Introduzca su nombre de usuario").toUpperCase()
+    Estatus.push(new Personaje(TipodePersonaje = prompt('Escoja su personaje... Escriba "Verde", "Azul" o "Rojo"').toUpperCase()))
+    
+    while (Camino > 0 || Seguirjugando != "N") {
+        
+        switch (Dado) {
             case 1, 3, 5:
-                Caminar()
                 Desplazamiento()
                 break;
         
             case 2, 4, 6:
-                console.log('¡Ha aparecido una criatura! Es un ' + Criaturas.indexOf[Math.random*5+1]);
+                console.log('¡Ha aparecido una criatura! Es un ' + Criaturas[Math.random*5+1]);
                 break;
         }
-    }while (Camino <= 0 || Criaturas.length == 0)
-} else {
-    alert('¡Adios! ¡Vuelve pronto!')
-}
+        let Seguirjugando = prompt('¿Quieres seguir? Escriba "Y" o "N"').toUpperCase()
+        if (Seguirjugando != "Y") break
+    }
+}   
+////RESPUESTA SI DICE QUE NO////
+alert('¡Adios! ¡Vuelve pronto!')
 
 
 
